1d2d2e3446e2ae0935589694078a19f5046b0dbd
Adrii-0906 <adrianranaescote4@gmail.com>
2025-11-24 19:27:05 +0100

    Desarrollando la interfaz del Restaurante

--- JavaFX/Restaurante/src/Restaurante/controllers/InventarioController.java
+++ JavaFX/Restaurante/src/Restaurante/controllers/InventarioController.java
@@ -1,4 +1,240 @@
 package Restaurante.controllers;
 
-public class InventarioController {
+import Restaurante.model.Producto;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.*;
+import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.input.MouseEvent;
+import javafx.stage.Stage;
+
+import java.io.*;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class InventarioController implements Initializable {
+
+    @FXML
+    private TextField txtNombre;
+    @FXML
+    private TextField txtCantidad;
+    @FXML
+    private TextField txtPrecio;
+
+    @FXML
+     private ComboBox<String> cbCategorias;
+
+    @FXML
+    private TableView<Producto> tablaProductos;
+
+    @FXML
+    private TableColumn<Producto, String> colNombre;
+    @FXML
+    private TableColumn<Producto, Integer> colCantidad;
+    @FXML
+    private TableColumn<Producto, Double> colPrecio;
+
+    @FXML
+    private TableColumn<Producto, String> colCategoria;
+
+
+    private ObservableList<Producto> listaProductos;
+    private String ficheroProductos = "/Restaurante/archivos_restaurante/productos.txt";
+
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        colNombre.setCellValueFactory(new PropertyValueFactory<>("nombre"));
+        colPrecio.setCellValueFactory(new PropertyValueFactory<>("precio"));
+        colCantidad.setCellValueFactory(new PropertyValueFactory<>("cantidad"));
+
+        colCategoria.setCellValueFactory(new PropertyValueFactory<>("categoria"));
+
+        cbCategorias.getItems().addAll("Bebida", "Entrante", "Carne", "Pescado", "Postre");
+
+        cbCategorias.getSelectionModel().selectFirst();
+
+        listaProductos = FXCollections.observableArrayList();
+        cargarProdcuctosDelFichero();
+        tablaProductos.setItems(listaProductos);
+    }
+
+    @FXML
+    public void anadirProductos(ActionEvent event) {
+        try {
+            String nombre = txtNombre.getText();
+            double precio = Double.parseDouble(txtPrecio.getText());
+            int cantidad = Integer.parseInt(txtCantidad.getText());
+
+            String categoria = cbCategorias.getValue();
+
+            if (nombre.isEmpty()) {
+                // Poner label para los errores
+                return;
+            }
+
+            Producto nuevoProducto = new Producto(nombre, precio, cantidad, categoria);
+
+            listaProductos.add(nuevoProducto);
+            guardarEnFichero(nuevoProducto);
+
+            limpiarCampos();
+
+
+
+        } catch (NumberFormatException e) {
+            throw new RuntimeException(e);
+            // Poner label para los errores
+        }
+    }
+
+
+    private void guardarEnFichero(Producto p) {
+        try {
+            BufferedWriter bw = new BufferedWriter(new FileWriter(ficheroProductos));
+            bw.write(p.getNombre() + "," + p.getPrecio() + "," + p.getCantidad() + "," + p.getCategoria());
+            bw.newLine();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+    private void cargarProdcuctosDelFichero() {
+        File fichero = new File(ficheroProductos);
+        if (!fichero.exists()) {
+            return;
+        }
+
+        try {
+            BufferedReader br = new BufferedReader(new FileReader(fichero));
+
+            String linea;
+
+            while ((linea = br.readLine()) != null) {
+                String[] partes = linea.split(",");
+
+                if (partes.length == 4) {
+                    String nombre = partes[0];
+                    double precio = Double.parseDouble(partes[1]);
+                    int cantidad = Integer.parseInt(partes[2]);
+                    String categoria = partes[3];
+
+                    listaProductos.add(new Producto(nombre, precio, cantidad, categoria));
+                }
+            }
+
+        }catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @FXML
+    public void seleccionarProdcutos(MouseEvent event) {
+        Producto p = tablaProductos.getSelectionModel().getSelectedItem();
+
+        if (p != null) {
+            txtNombre.setText(p.getNombre());
+            txtPrecio.setText(String.valueOf(p.getPrecio()));
+            txtCantidad.setText(String.valueOf(p.getCantidad()));
+            cbCategorias.setValue(p.getCategoria());
+        }
+    }
+
+    @FXML
+    public void modificarProductos(ActionEvent event) {
+        Producto p = tablaProductos.getSelectionModel().getSelectedItem();
+
+        if (p == null) {
+            // Poner el label de error
+            return;
+        }
+
+        try {
+            p.setNombre(txtNombre.getText());
+            p.setPrecio(Double.parseDouble(txtPrecio.getText()));
+            p.setCantidad(Integer.parseInt(txtCantidad.getText()));
+            p.setCategoria(cbCategorias.getValue());
+
+            tablaProductos.refresh();
+
+            reescribirFichero();
+
+            // Mostrar label con un mensaje de exito
+            limpiarCampos();
+
+        } catch (NumberFormatException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @FXML
+    public void eliminarProducto(ActionEvent event) {
+        Producto p = tablaProductos.getSelectionModel().getSelectedItem();
+
+        if (p == null) {
+            // Poner el label de error
+            return;
+        }
+
+        Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
+        alert.setHeaderText(null);
+        alert.setTitle("Confirmación");
+        alert.setContentText("¿Seguro que quieres borrar " + p.getNombre() + "?");
+
+        if (alert.showAndWait().get() == ButtonType.OK) {
+            listaProductos.remove(p);
+
+            reescribirFichero();
+
+            // Mostrar label con un mensaje de exito
+            limpiarCampos();
+        }
+    }
+
+    public void reescribirFichero() {
+
+        try {
+            BufferedWriter bw = new BufferedWriter(new FileWriter(ficheroProductos));
+
+            for (Producto p : listaProductos) {
+                bw.write(p.getNombre() + "," + p.getPrecio() + "," + p.getCantidad() + "," + p.getCategoria());
+                bw.newLine();
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void limpiarCampos() {
+        txtNombre.clear();
+        txtPrecio.clear();
+        txtCantidad.clear();
+        cbCategorias.getSelectionModel().selectFirst();
+    }
+
+
+    public void volverAInicio(MouseEvent event) {
+        try {
+            Parent root = FXMLLoader.load(getClass().getResource("/Restaurante/vistas/pantallaPrincipal.fxml"));
+            Stage stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+
+            stage.setTitle("Restaurante Casa Manolo");
+            stage.setScene(new Scene(root));
+            stage.centerOnScreen(); // Opcional: Centrar ventana
+            stage.show();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
 }
--- JavaFX/Restaurante/src/Restaurante/controllers/PantallaPrincipalController.java
+++ JavaFX/Restaurante/src/Restaurante/controllers/PantallaPrincipalController.java
@@ -1,4 +1,100 @@
 package Restaurante.controllers;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
 public class PantallaPrincipalController {
+
+    @FXML
+    private ImageView mesa1;
+    @FXML
+    private ImageView mesa2;
+    @FXML
+    private ImageView mesa3;
+    @FXML
+    private ImageView mesa4;
+    @FXML
+    private ImageView mesa5;
+    @FXML
+    private ImageView mesa6;
+    @FXML
+    private ImageView mesa7;
+    @FXML
+    private ImageView mesa8;
+    @FXML
+    private ImageView mesa9;
+
+    @FXML
+    private Label btnInventario;
+    @FXML
+    private Label btnCaja;
+
+
+    private void cambiarPantalla(MouseEvent event, String fxml, String titulo) {
+        try {
+            Parent root = FXMLLoader.load(getClass().getResource(fxml));
+            Stage stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+            stage.setTitle(titulo);
+            stage.setScene(new Scene(root));
+            stage.centerOnScreen();
+            stage.show();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @FXML
+    public void irAMesa1(MouseEvent event) {
+        cambiarPantalla(event, "/Restaurante/vistas/comanda1.fxml", "Comanda Mesa 1");
+    }
+    @FXML
+    public void irAMesa2(MouseEvent event) {
+        cambiarPantalla(event, "/Restaurante/vistas/comanda1.fxml", "Comanda Mesa 2");
+    }
+    @FXML
+    public void irAMesa3(MouseEvent event) {
+        cambiarPantalla(event, "/Restaurante/vistas/comanda1.fxml", "Comanda Mesa 3");
+    }
+    @FXML
+    public void irAMesa4(MouseEvent event) {
+        cambiarPantalla(event, "/Restaurante/vistas/comanda1.fxml", "Comanda Mesa 4");
+    }
+    @FXML
+    public void irAMesa5(MouseEvent event) {
+        cambiarPantalla(event, "/Restaurante/vistas/comanda1.fxml", "Comanda Mesa 5");
+    }
+    @FXML
+    public void irAMesa6(MouseEvent event) {
+        cambiarPantalla(event, "/Restaurante/vistas/comanda1.fxml", "Comanda Mesa 6");
+    }
+    @FXML
+    public void irAMesa7(MouseEvent event) {
+        cambiarPantalla(event, "/Restaurante/vistas/comanda1.fxml", "Comanda Mesa 7");
+    }
+    @FXML
+    public void irAMesa8(MouseEvent event) {
+        cambiarPantalla(event, "/Restaurante/vistas/comanda1.fxml", "Comanda Mesa 8");
+    }
+    @FXML
+    public void irAMesa9(MouseEvent event) {
+        cambiarPantalla(event, "/Restaurante/vistas/comanda1.fxml", "Comanda Mesa 9");
+    }
+
+    public void irAInventario(MouseEvent event) {
+        cambiarPantalla(event, "/Restaurante/vistas/inventario.fxml", "Iventario Restaurante");
+    }
+
+    public void irACaja(MouseEvent event) {
+        cambiarPantalla(event, "", "Caja Restaurante");
+    }
 }
